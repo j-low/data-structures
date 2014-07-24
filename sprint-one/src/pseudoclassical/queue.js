c4ce632269a07712b76993b0e0e6774b2150456f
lowi0008 <lowi0008@gmail.com>
2014-07-24 16:41:14 -0700

    data structures: all complete

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,29 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+ this.index = 0;
+ this.front = 0;
 };
 
+Queue.prototype.enqueue = function(value) {
+  this[this.index + this.front] = value;
+  this.index++;
+};
 
+Queue.prototype.dequeue = function() {
+  if(this.index > 0) {
+    this.index--;
+  }
+
+  var result = this[this.front];
+  delete this[this.front];
+
+  this.front++;
+
+  return result;
+
+};
+
+Queue.prototype.size = function() {
+  return this.index;
+};
