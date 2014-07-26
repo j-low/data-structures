149e0b9214a408b449fec00c78fea20181561fb9
lowi0008 <lowi0008@gmail.com>
2014-07-25 23:19:37 -0700

    hash tables and linked lists complete

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -4,15 +4,58 @@ var HashTable = function(){
 };
 
 HashTable.prototype.insert = function(k, v){
-  var i = getIndexBelowMaxForKey(k, this._limit);
+  var i = getIndexBelowMaxForKey(k, this._limit)
+  var bucket = this._storage.get(i);
+
+  if(!bucket) {
+    this._storage.set(i, [[k,v]]);
+  } else {
+    bucket.push([k,v]);
+  }
+
+  // this._storage.each(function(storageI , i , storage) {
+  //   if (storageI){
+  //     console.log(storageI)
+  //     console.log(i);
+  //     console.log(storage);
+  //   }
+  // })
+
+
+  // this._storage.set(i, value);
+
+
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(i);
+
+  for(var j = 0; j < bucket.length; j++) {
+    //check k === key in sub arrays of bucket
+    if(bucket[j][0] === k) {
+      return bucket[j][1];
+    }
+  }
+
+  // bucket.each(function(subArr) {
+  //   if(subArr[0] === k) {
+  //     return subArr[1];
+  //   }
+  // });
+
 
 };
 
 HashTable.prototype.remove = function(k){
+  var i = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(i);
+
+  for(var j = 0; j < bucket.length; j++ ) {
+    if(bucket[j][0] === k) {
+      bucket[j][1] = null;
+    }
+  }
 
 };
 
--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,40 @@ var makeLinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    var newNode = makeNode(value);
+
+    if(this.head === null){
+      this.head = newNode;
+      this.tail = newNode;
+    }
+    else{
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+
   };
 
   list.removeHead = function(){
+    var choppedHead = this.head.value;
+    var newHead = this.head.next;
+    this.head = newHead;
+    return choppedHead;
   };
 
   list.contains = function(target){
+    var current = this.head;
+    if(current.value === target){
+      return true;
+    }
+    else{
+    while(current.next){
+      current = current.next;
+      if(current.value === target) {
+        return true;
+      }
+    }
+  }
+    return false;
   };
 
   return list;
