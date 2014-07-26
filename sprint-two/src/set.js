481909e77edf8dcddea4e043359b94a86a518ba2
lowi0008 <lowi0008@gmail.com>
2014-07-25 23:58:11 -0700

    set completed; tree in progress

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -7,12 +7,19 @@ var makeSet = function(){
 var setPrototype = {};
 
 setPrototype.add = function(item){
+  this[item] = item;
 };
 
 setPrototype.contains = function(item){
+  if(item in this) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 setPrototype.remove = function(item){
+  delete this[item];
 };
 
 /*
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,24 +1,38 @@
 var makeTree = function(value){
-  var newTree = {};
+  var newTree = Object.create(treeMethods);
   newTree.value = value;
-  newTree.children = undefined;
+  newTree.children = [];
   return newTree;
 };
-
-
 
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
 
+  this.children.push(makeTree(value));
 };
 
 treeMethods.contains = function(target){
-
+  if(this.value === target) {
+    return true;
+  } else {
+    if(this.children) {
+      for(var child in this.children) {
+        child.contains(target);
+      }
+    } else {
+      return false;
+    }
+  }
 };
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+
+//var startNode = makeTree(1);
+//startNode.addChild(2);
+//startNode.children
